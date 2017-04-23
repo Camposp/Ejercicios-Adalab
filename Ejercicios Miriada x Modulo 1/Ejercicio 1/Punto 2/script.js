e3988bf8cb82ae560a03fc46eed2d3845fa63c59
Camposp <elisacampospousa@gmail.com>
2017-04-23 20:28:55 +0200

    Update script.js
    Add information in console.log()

--- Ejercicios Miriada x Modulo 1/Ejercicio 1/Punto 2/script.js
+++ Ejercicios Miriada x Modulo 1/Ejercicio 1/Punto 2/script.js
@@ -1,5 +1,5 @@
 var baraja=52;
 var paloNegro=(baraja/2);
-console.log(paloNegro);
+console.log("Las cartas con palo negro son:" +paloNegro);
 var par=(paloNegro/2);
-console.log(par);
+console.log("Las cartas con palo negro y pares son:"+par);

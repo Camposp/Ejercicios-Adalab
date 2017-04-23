6a88514ba769c827c578c182a9f6a0212cfd7a93
Camposp <elisacampospousa@gmail.com>
2017-04-23 20:31:00 +0200

    Update script.js
    Add a space between : and "

--- Ejercicios Miriada x Modulo 1/Ejercicio 1/Punto 2/script.js
+++ Ejercicios Miriada x Modulo 1/Ejercicio 1/Punto 2/script.js
@@ -1,5 +1,5 @@
 var baraja=52;
 var paloNegro=(baraja/2);
-console.log("Las cartas con palo negro son:" +paloNegro);
+console.log("Las cartas con palo negro son: " +paloNegro);
 var par=(paloNegro/2);
-console.log("Las cartas con palo negro y pares son:"+par);
+console.log("Las cartas con palo negro y pares son: "+par);
